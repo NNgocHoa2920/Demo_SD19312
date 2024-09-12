7c57ba44927015682ad8008cc821f3e8d1be0e2a
nguye_ux <nguye_ux@NGUYEN_NGOC_HOA>
2024-09-12 08:34:24 +0700

    Add project files.

---
+++ MVC_Web/Models/SD19312DbContext.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Web.Models
+{
+    //class này đại diện cho sql server ở trong visual studio
+    public class SD19312DbContext : DbContext
+    {
+        public SD19312DbContext()
+        {
+        }
+
+        public SD19312DbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //dbset đại diện cho 1 thực thể, 1 bảng ở trong database
+        //khi đụng db thì gọi đến thực thể này chứ k phải class
+        public DbSet<Category> Categories{ get; set; }
+        //cách 1: để chuỗi kết nối ở đây
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=NGUYEN_NGOC_HOA\\SQLEXPRESS; Database=DbSD19312" +
+                ";Trusted_Connection = True; TrustServerCertificate= True");
+        }
+
+        
+
+    }
+}
---
+++ MVC_Web/Program.cs
@@ -0,0 +1,33 @@
+﻿var builder = WebApplication.CreateBuilder(args); //tạo ra ứng dụng web
+
+// Add services to the container.
+builder.Services.AddControllersWithViews(); // add các service cần
+//sau này service thì bắt buộc phải add dưới var builder và trên var app
+
+var app = builder.Build(); // chạy đối tượng web
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) // Kiểm tra co phải môi trường dev hay k
+{
+    app.UseExceptionHandler("/Home/Error");  //thiết lập đường dẫn
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();// cấu hình và điều hướng vs https
+app.UseStaticFiles(); //sử dụng tài nguyên tĩnh/ middleware phục vụ tập tĩnh
+
+app.UseRouting();//cấu hình đinh tuyến/ middleware định tuyến
+
+app.UseAuthorization();//cơ chế chứng thực- phân quyền
+                //su này thiết lập theo sesion colli hay người dùng
+                 //middleware về xác thực
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// cáu hình cach thức truy cập action của controler
+//htts:/local123/controler/action/id
+app.Run();
