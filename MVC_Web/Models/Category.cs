2a40001bb38c11f763155cb453d62757474909d2
Hoa Nguyễn Ngọc <nguyenngochoa292000@gmail.com>
2024-09-19 09:28:37 +0700

    1

--- MVC_Web/Models/Category.cs
+++ MVC_Web/Models/Category.cs
@@ -1,9 +1,14 @@
-﻿namespace MVC_Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Web.Models
 {
     public class Category
     {
+        [Key]
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Range(1,100,ErrorMessage ="Nhập trong khoảng từ 1-100")]
         public int DisplayOrder {  get; set; }
     }
 }
